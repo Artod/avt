56a061c32c36b1dce2bd46b9721dec0ccab8f001
Artem <gartod@gmail.com>
2011-05-03 16:16:11 +0400

    Fin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+City.create([{
+    :name => 'Moscow',
+    :latitude => '55.755790',
+    :longitude => '37.617630'
+  }, {
+    :name => 'Bangkok',
+    :latitude => '13.723420',
+    :longitude => '100.476230'
+}])
+
+Cost.create([{
+    :origin_id => 1,
+    :destination_id => 2,
+    :depart_date => '2011-05-01',
+    :return_date => '2011-05-10',
+    :value => '1000'
+  }, {
+    :origin_id => 1,
+    :destination_id => 2,
+    :depart_date => '2011-05-01',
+    :return_date => '2011-05-15',
+    :value => '1500'
+}])
+
